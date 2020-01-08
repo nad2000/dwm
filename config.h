ef82fad8ddeb20bfa29a4c21d2b0ec838b94bead
Rad Cirskis <nad2000@gmail.com>
2020-01-09 00:40:27 +1300

    adde slock directly to dwm

--- config.h
+++ config.h
@@ -67,9 +67,11 @@ static const char *dmenucmd[] = {
     "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
     "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
 static const char *termcmd[] = {"st", NULL};
+static const char *slockcmd[] = {"slock", NULL};
 
 static Key keys[] = {
     /* modifier                     key        function        argument */
+    {MODKEY | ShiftMask, XK_l, spawn, {.v = slockcmd}},
     {MODKEY, XK_p, spawn, {.v = dmenucmd}},
     {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
     {MODKEY, XK_b, togglebar, {0}},
