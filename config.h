7dbd1945bebedae95076d65dd9f2c5492c9afe7a
Rad Cirskis <nad2000@gmail.com>
2023-04-27 13:41:35 +0800

    better font sizing form 15inch screen

--- config.h
+++ config.h
@@ -1,16 +1,19 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 1; /* border pixel of windows */
+static const unsigned int borderpx = 0;  // 1 /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[] = {"monospace:size=10"};
-static const char dmenufont[] = "monospace:size=10";
+// static const char *fonts[] = {"monospace:size=10"};
+static const char *fonts[] = {"monospace:size=12"};
+// static const char dmenufont[] = "monospace:size=10";
+static const char dmenufont[] = "monospace:size=11";
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
 static const char col_gray4[] = "#eeeeee";
+// static const char col_cyan[] = "#005577";
 static const char col_cyan[] = "#005577";
 static const unsigned int baralpha = 0xc0;
 /* static const unsigned int borderalpha = OPAQUE; */
@@ -32,11 +35,12 @@ static const Rule rules[] = {
      */
     /* class  instance  title   tags mask   switchtotag   isfloating   monitor
      */
+    // {NULL, NULL, NULL, 0, 0, 0, -1},
     {"Gimp", NULL, NULL, 0, 0, 1, -1},
     {"Firefox", NULL, NULL, 1 << 8, 0, 0, -1},
     // {"Slack", NULL, "slack", 1 << 8, 0, -1},
-    {"Slack", NULL, NULL, 1 << 8, 0, 0, 0},
-    {"trayer", NULL, "panel", 1 << 8, 0, 0, 0},
+    {"Slack", NULL, NULL, 1 << 8, 0, 0, -1},
+    {"trayer", NULL, "panel", 1 << 8, 0, 0, -1},
     {"blueman-manager", NULL, "Bluetooth Devices", 1 << 7, 0, 0, 0},
 };
 
@@ -66,7 +70,7 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd)                                                             \
   {                                                                            \
-    .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL }                       \
+     = (const char *[]) { "/bin/sh", "-c", cmd, NULL }                       \
   }
 
 /* commands */
@@ -77,12 +81,14 @@ static const char *dmenucmd[] = {
     "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
 static const char *termcmd[] = {"st", NULL};
 static const char *slockcmd[] = {"slock", NULL};
+static const char *tabbed_surf[] = {"tabbed", "-c", "surf", "-e", NULL};
 
 static Key keys[] = {
     /* modifier                     key        function        argument */
     {MODKEY | ShiftMask, XK_l, spawn, {.v = slockcmd}},
     {MODKEY, XK_p, spawn, {.v = dmenucmd}},
     {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
+    {MODKEY | ShiftMask, XK_s, spawn, {.v = tabbed_surf}},
     {MODKEY, XK_b, togglebar, {0}},
     {MODKEY, XK_j, focusstack, {.i = +1}},
     {MODKEY, XK_k, focusstack, {.i = -1}},
