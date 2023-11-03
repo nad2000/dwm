d21dd98835168e9ac80b667459e7934a95d2821a
Rad Cirskis <nad2000@gmail.com>
2023-11-03 16:24:36 +1300

    this appears the best font

--- config.h
+++ config.h
@@ -5,10 +5,13 @@ static const unsigned int borderpx = 0;  // 1 /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
+// #define FONT "Hack"
+#define FONT "Victor Mono Nerd Font"
 // static const char *fonts[] = {"monospace:size=10"};
-static const char *fonts[] = {"monospace:size=12"};
+// static const char *fonts[] = {"monospace:size=12"};
+static const char *fonts[] = {FONT ":size=12", "monospace:size=12"};
 // static const char dmenufont[] = "monospace:size=10";
-static const char dmenufont[] = "monospace:size=11";
+static const char dmenufont[] = FONT ":size=11";
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
